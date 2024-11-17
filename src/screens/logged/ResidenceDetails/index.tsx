80e2d11523516bf4035bdfd77133ef97d9693596
viniciusandradee <vini.aclopes@gmail.com>
2024-11-17 18:10:15 -0300

    ResidenceDetails without solarPanel infos and energy saving

--- src/screens/logged/ResidenceDetails/index.tsx
+++ src/screens/logged/ResidenceDetails/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, ScrollView, TextInput } from 'react-native';
 
 import styles from './style';
 
@@ -8,6 +8,7 @@ import { RouteProp } from '@react-navigation/native';
 import { LoggedDrawer } from '@/types';  // Importe o tipo que você já definiu
 import { database } from 'firebaseConfig';
 import { doc, getDoc } from 'firebase/firestore';
+import colors from '@/styles/colors';
 
 type ResidenceDetailsRouteProp = RouteProp<LoggedDrawer, 'ResidenceDetails'>;
 
@@ -81,7 +82,46 @@ const ResidenceDetails = () => {
                     </View>
 
                     <View style={styles.main}>
-                        {/* Outros detalhes da residência */}
+
+                        <View style={styles.energyBillContainer}>
+                            <Text style={styles.energyBillText}>Current energy bill</Text>
+                            <TextInput
+                                style={styles.input}
+                                placeholder='Type here...'
+                                placeholderTextColor={'#6A6767'}
+                            />
+                            <View style={styles.line}></View>
+
+                            <Text style={styles.savingsText}>With solar energy, your savings would be (Y - (30 to 50)) per month.</Text>
+                            <Text style={styles.savingsText2}>≅ 80% to 90% savings</Text>
+                        </View>
+
+
+                        <View style={styles.sustentableEnergyContainer}>
+                            <View style={styles.helpContainer}>
+                                <Text style={styles.co2Text}>You are helping to avoid the emission of 180 kg of CO₂ per year!</Text>
+                            </View>
+                            <View style={styles.helpContainer}>
+                                <Text style={styles.economyText}>Globally: Avoiding 900 kgCO₂/year is equivalent to:</Text>
+                                <Text style={styles.economyText}>• Do not <Text style={styles.spanDrive}>drive a car</Text> for 6 months.</Text>
+                                <Text style={styles.economyText}>• Plant around 40 <Text style={styles.spanTree}>trees</Text>.</Text>
+
+                                <Text style={styles.economyText}>In Brazil: Avoiding 180 kgCO₂/year is equivalent to:</Text>
+                                <Text style={styles.economyText}>• Do not <Text style={styles.spanDrive}>drive</Text> for 1 month.</Text>
+                                <Text style={styles.economyText}>• Plant about 8 <Text style={styles.spanTree}>trees</Text>.</Text>
+                            </View>
+                        </View>
+
+                        <View style={styles.solarPanelEconomyContainer}>
+                            <Text style={styles.solarPanelTitle}>Economy with solar panels</Text>
+                            <Text style={styles.solarPanelText}>
+                                Sistema típico de <Text style={styles.spanEnergy}>3 kWp</Text>:{"\n"}
+                                • Custo médio: R$ 18.000.{"\n"}
+                                • Energia gerada mensalmente: 360 a 450 kWh.{"\n"}
+                                • Economia média mensal: Custo do kWh: R$ 0,70/kWh, seria ~R$ 250 a R$ 300/mês.{"\n"}
+                                • Retorno do investimento: Cerca de 5 a 7 anos, dependendo do consumo e da tarifa local.
+                            </Text>
+                        </View>
                     </View>
 
                     <View style={styles.footer}>
--- src/screens/logged/ResidenceDetails/style.ts
+++ src/screens/logged/ResidenceDetails/style.ts
@@ -82,6 +82,118 @@ const styles = StyleSheet.create({
         marginTop: 40
     },
 
+    energyBillContainer: {
+        backgroundColor: colors.tertiary,
+        width: '90%',
+        height: 200,
+        borderRadius: 6,
+
+        alignItems: 'center'
+    },
+
+    energyBillText: {
+        marginTop: 15,
+        textAlign: 'center',
+        color: colors.quinary,
+        fontSize: 18,
+    },
+
+    input: {
+        marginTop: 10,
+        backgroundColor: colors.quinary,
+        width: '45%',
+        height: 20,
+        borderRadius: 4,
+        paddingLeft: 10,
+        fontSize: 10,
+    },
+
+    line: {
+        marginTop: 20,
+        backgroundColor: colors.quinary,
+        height: 1,
+        width: '90%'
+    },
+
+    savingsText: {
+        marginTop: 20,
+        textAlign: 'center',
+        fontSize: 12,
+        color: colors.quinary
+    },
+    savingsText2: {
+        marginTop: 15,
+        textAlign: 'center',
+        fontSize: 12,
+        color: colors.quinary
+
+    },
+
+    sustentableEnergyContainer: {
+        marginTop: 30,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-around',  // Distribui o espaço entre os filhos
+        width: '100%'
+    },
+
+    helpContainer: {
+        width: '40%',  // Aumentando a largura para garantir espaço entre eles
+        height: 180,
+        backgroundColor: colors.tertiary,
+        borderRadius: 6,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+
+    co2Text: {
+        textAlign: 'center',
+        color: colors.quinary,
+        fontSize: 12,
+        paddingHorizontal: 5
+    },
+
+    economyText: {
+        textAlign: 'center',
+        color: colors.quinary,
+        fontSize: 10,
+        paddingHorizontal: 5
+    },
+
+    spanDrive: {
+        color: '#C1121F'
+    },
+
+    spanTree: {
+        color: '#13D447'
+    },
+
+    solarPanelEconomyContainer: {
+        marginTop: 30,
+        marginBottom: 40,
+        backgroundColor: colors.tertiary,
+        width: '90%',
+        height: 200,
+        borderRadius: 6,
+
+        alignItems: 'center'
+    },
+
+    solarPanelTitle: {
+        marginTop: 10,
+        color: colors.quinary,
+        fontSize: 20
+    },
+
+    solarPanelText: {
+        marginTop: 10,
+        width: '85%'
+    },
+
+    spanEnergy: {
+        color: 'yellow'
+    },
 
     // FOOTER
 
